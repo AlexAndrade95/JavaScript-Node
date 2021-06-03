5d96356107e394771142a77d414054e3a553c9fc
AlexAndrade95 <alexneves19955@gmail.com>
2021-06-03 16:15:11 -0300

    commit 05 versao 1.1

---
+++ 07exportar.js
@@ -0,0 +1,25 @@
+console.log(module.exports)
+//{}
+console.log(module.exports === this)
+//true
+console.log(module.exports === exports)
+//true
+
+this.a = 1
+exports.b = 2
+module.exports.c = 3
+
+exports = null
+console.log(module.exports)
+//{ a: 1, b: 2, c: 3 }
+
+exports = {
+    nome: 'Teste'
+}
+
+console.log(module.exports)
+//{ a: 1, b: 2, c: 3 }
+
+module.exports = { publico: true }
+
+//na exportacao sera retornado o que esta no module.exports
